731b692d7fb409b0ce3d7ab00a91e94dbabc8a11
estevaofon <estevaopfon@gmail.com>
2017-11-29 23:51:31 -0200

    updated version

--- pyanimation/__init__.py
+++ pyanimation/__init__.py
@@ -3,6 +3,6 @@ from .animation import Animation
 __title__ = 'pyanimation'
 __description__ = 'A python module to easily create animations'
 __url__ = 'https://github.com/estevaofon/pyanimation'
-__version__ = '0.6'
+__version__ = '0.7'
 __author__ = 'Estevao Fonseca'
 __license__ = 'MIT'
